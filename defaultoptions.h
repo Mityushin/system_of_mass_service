7f3ec6266ccfc8016bf4de511d6ef8c7c5ef296f
Dmitry Mityushin <mityushin.dmitry@gmail.com>
2017-11-23 04:58:53 +0300

    add distribution laws

--- defaultoptions.h
+++ defaultoptions.h
@@ -1,9 +1,15 @@
 #ifndef DEFAULTOPTIONS_H
 #define DEFAULTOPTIONS_H
 
-const unsigned int DEFAULT_NUMBER_OF_SOURCES = 3;
+const unsigned int DEFAULT_NUMBER_OF_BIDS = 10;
+
+const unsigned int DEFAULT_NUMBER_OF_SOURCES = 7;
 const unsigned int DEFAULT_NUMBER_OF_DEVICES = 5;
 const unsigned int DEFAULT_BUFFER_CAPACITY = 4;
+
+const long double DEFAULT_ALPHA = 1.0;
+const long double DEFAULT_BETA = 1.0;
+const long double DEFAULT_LAMBDA = 0.12;
 
 const long double DEFAULT_PROCESSING_TIME = 1.0;
 
